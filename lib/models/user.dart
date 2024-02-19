a61efa8587c98b0c13c563820fa43f09c25d99e7
ByWizKi <enzoth39260@gmail.com>
2024-02-19 22:38:51 +0100

    issue UserClass : Done 100% All Test Passed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,38 +1,116 @@
-// ignore_for_file: unnecessary_getters_setters
+// ignore_for_file: unnecessary_getters_setters, avoid_print
 
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+/// Classe User pour représenter un utilisateur avec ses informations géographiques et temporelles.
 class User {
-  int _age;
+  int _age = 18;
   double _longitude;
   double _latitude;
-  String _city;
-  DateTime _pickUpDate;
-  DateTime _dropOffDate;
+  DateTime _datePickUp = DateTime.now();
+  DateTime _dateDropOff = DateTime.now().add(const Duration(days: 1));
 
-  User({
-    int age = 0,
-    required double longitude,
-    required double latitude,
-    required String city,
-  })  : _age = age,
-        _longitude = longitude,
-        _latitude = latitude,
-        _city = city,
-        _pickUpDate = DateTime.now(),
-        _dropOffDate = DateTime.now().add(Duration(days: 1));
+  /// Construit un [User] avec la [longitude] et la [latitude] spécifiées.
+  User({required double longitude, required double latitude})
+      : _longitude = longitude,
+        _latitude = latitude;
 
-  // Getters
+  /// Obtient l'âge de l'utilisateur.
   int get age => _age;
-  double get longitude => _longitude;
-  double get latitude => _latitude;
-  String get city => _city;
-  DateTime get pickUpDate => _pickUpDate;
-  DateTime get dropOffDate => _dropOffDate;
 
-  // Setters
-  set age(int value) => _age = value;
+  /// Obtient la date de prise en charge.
+  DateTime get datePickUp => _datePickUp;
+
+  /// Obtient la date de retour.
+  DateTime get dateDropOff => _dateDropOff;
+
+  /// Obtient la longitude de l'utilisateur.
+  double get longitude => _longitude;
+
+  /// Obtient la latitude de l'utilisateur.
+  double get latitude => _latitude;
+
+  /// Définit la date de prise en charge.
+  /// Lève une exception si la date est dans le passé.
+  set datePickUp(DateTime value) {
+    if (value.isBefore(DateTime.now())) {
+      throw Exception(
+          'La date de prise en charge ne peut pas être dans le passé.');
+    }
+    _datePickUp = value;
+  }
+
+  /// Définit la date de retour.
+  /// Lève une exception si la date est avant la date de prise en charge.
+  set dateDropOff(DateTime value) {
+    if (value.isBefore(_datePickUp)) {
+      throw Exception(
+          'La date de retour doit être après la date de prise en charge.');
+    }
+    _dateDropOff = value;
+  }
+
+  /// Définit l'âge de l'utilisateur.
+  /// Lève une exception si l'âge est négatif.
+  set age(int value) {
+    if (value > 0) {
+      _age = value;
+    } else {
+      throw ArgumentError("L'âge doit être positif");
+    }
+  }
+
+  /// Définit la longitude de l'utilisateur.
   set longitude(double value) => _longitude = value;
+
+  /// Définit la latitude de l'utilisateur.
   set latitude(double value) => _latitude = value;
-  set city(String value) => _city = value;
-  set pickUpDate(DateTime value) => _pickUpDate = value;
-  set dropOffDate(DateTime value) => _dropOffDate = value;
+
+  /// Convertit les coordonnées géographiques en nom de ville.
+  /// Retourne le nom de la ville ou lève une exception si la requête échoue.
+  Future<String?> coordinatesToCity() async {
+    var url = Uri.parse(
+        'https://nominatim.openstreetmap.org/reverse?format=json&lat=$_latitude&lon=$_longitude');
+
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        return data['address']['city'] ?? 'Ville inconnue';
+      } else {
+        print('Erreur de réponse : ${response.statusCode}');
+        return 'Échec de la récupération du nom de la ville';
+      }
+    } catch (e) {
+      print('Exception attrapée : $e');
+      return 'Erreur lors de la récupération du nom de la ville';
+    }
+  }
+
+   /// Définit les coordonnées de l'utilisateur en fonction du nom de la ville.
+  /// Utilise l'API Nominatim pour le géocodage inversé.
+  ///
+  /// [city] Le nom de la ville pour laquelle récupérer les coordonnées.
+  Future<void> setCoordinatesFromCity(String city) async {
+    var url = Uri.parse('https://nominatim.openstreetmap.org/search?city=$city&format=json&limit=1');
+
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        if (data.isNotEmpty) {
+          _latitude = double.parse(data[0]['lat']);
+          _longitude = double.parse(data[0]['lon']);
+        } else {
+          print('Aucun résultat trouvé pour cette ville');
+        }
+      } else {
+        print('Erreur de réponse : ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Exception attrapée : $e');
+    }
+  }
 }
--- test/user_test.dart
+++ test/user_test.dart
@@ -1,49 +1,61 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:best_rent/models/user.dart'; 
+import 'package:best_rent/models/user.dart'; // Remplacez par le chemin correct vers votre classe User.
+import 'package:test/test.dart';
 
 void main() {
-  group('User Accessors and Mutators', () {
+  group('User tests', () {
     late User user;
-
     setUp(() {
-      user = User(
-        age: 30,
-        longitude: 40.7128,
-        latitude: -74.0060,
-        city: 'New York',
-      );
+      // Initialiser l'utilisateur avant chaque test
+      user = User(longitude: 0.0, latitude: 0.0);
     });
 
-    test('should allow reading and modifying the age', () {
-      expect(user.age, 30);
+    test('Initial age should be 18', () {
+      expect(user.age, 18);
+    });
+
+    test('Setting age to a positive value', () {
       user.age = 25;
       expect(user.age, 25);
     });
 
-    test('should allow reading and modifying the longitude and latitude', () {
-      expect(user.longitude, 40.7128);
-      expect(user.latitude, -74.0060);
-      user.longitude = 34.0522; // Example: Los Angeles longitude
-      user.latitude = -118.2437; // Example: Los Angeles latitude
-      expect(user.longitude, 34.0522);
-      expect(user.latitude, -118.2437);
+    test('Setting age to a negative value should throw an error', () {
+      expect(() => user.age = -5, throwsA(isA<ArgumentError>()));
     });
 
-    test('should allow reading and modifying the city', () {
-      expect(user.city, 'New York');
-      user.city = 'Los Angeles';
-      expect(user.city, 'Los Angeles');
+    test('Setting datePickUp to a future date', () {
+      var futureDate = DateTime.now().add(const Duration(days: 2));
+      user.datePickUp = futureDate;
+      expect(user.datePickUp, futureDate);
     });
 
-    test('should allow reading and modifying the pickUpDate and dropOffDate', () {
-      final newPickUpDate = DateTime.now().add(Duration(days: 5));
-      final newDropOffDate = DateTime.now().add(Duration(days: 10));
-      expect(user.pickUpDate.day, DateTime.now().day);
-      expect(user.dropOffDate.day, DateTime.now().add(Duration(days: 1)).day);
-      user.pickUpDate = newPickUpDate;
-      user.dropOffDate = newDropOffDate;
-      expect(user.pickUpDate, newPickUpDate);
-      expect(user.dropOffDate, newDropOffDate);
+    test('Setting datePickUp to a past date should throw an error', () {
+      var pastDate = DateTime.now().subtract(const Duration(days: 1));
+      expect(() => user.datePickUp = pastDate, throwsException);
     });
+
+    test('coordinatesToCity returns a valid city name', () async {
+      // Assurez-vous d'avoir les bonnes coordonnées pour le test
+      user.longitude = -0.1278; // Longitude de Londres
+      user.latitude = 51.5074; // Latitude de Londres
+
+      var cityName = await user.coordinatesToCity();
+      expect(cityName, isNotNull);
+
+      // Imprimer le nom de la ville si le test réussit
+      // ignore: avoid_print
+      print('La ville trouvée est : $cityName');
+    });
+  });
+
+  test('Mise à jour des coordonnées pour une ville valide', () async {
+    User user = User(longitude: 0.0, latitude: 0.0);
+    await user.setCoordinatesFromCity('Paris');
+
+    print('Latitude: ${user.latitude}, Longitude: ${user.longitude}');
+
+    // Vérifiez que les coordonnées ont été mises à jour.
+    // Ces valeurs sont des exemples et doivent être ajustées en fonction des résultats réels attendus.
+    expect(user.latitude, isNot(0.0));
+    expect(user.longitude, isNot(0.0));
   });
 }
